69137acccd1b20a413abd06e8c5047289f821e42
Mohammad <nasiri.m@gmail.com>
2026-01-01 13:32:55 +0100

    feat(ui): reset learning

--- crates/ui/src/views/history.rs
+++ crates/ui/src/views/history.rs
@@ -1,5 +1,6 @@
 use dioxus::prelude::*;
 use dioxus_router::use_navigator;
+use learn_core::model::DeckId;
 
 use crate::context::AppContext;
 use crate::routes::Route;
@@ -11,6 +12,13 @@ struct HistoryData {
     cards: Vec<SessionSummaryCardVm>,
 }
 
+#[derive(Clone, Copy, Debug, PartialEq, Eq)]
+enum ResetState {
+    Idle,
+    Resetting,
+    Error(ViewError),
+}
+
 #[component]
 pub fn HistoryView() -> Element {
     let ctx = use_context::<AppContext>();
@@ -19,6 +27,9 @@ pub fn HistoryView() -> Element {
     let deck_service = ctx.deck_service();
     let card_service = ctx.card_service();
     let mut search = use_signal(String::new);
+    let mut open_menu = use_signal(|| None::<i64>);
+    let mut reset_target = use_signal(|| None::<u64>);
+    let mut reset_state = use_signal(|| ResetState::Idle);
 
     let resource = use_resource(move || {
         let summaries = summaries.clone();
@@ -41,8 +52,9 @@ pub fn HistoryView() -> Element {
             Ok::<_, ViewError>(deck.map(|deck| deck.name().to_string()))
         }
     });
+    let card_service_for_counts = card_service.clone();
     let counts_resource = use_resource(move || {
-        let card_service = card_service.clone();
+        let card_service = card_service_for_counts.clone();
         async move {
             let stats = card_service
                 .deck_practice_stats(deck_id)
@@ -103,6 +115,16 @@ pub fn HistoryView() -> Element {
                     };
                     let deck_label = deck_label.clone();
                     let due_label = due_label.clone();
+                    let on_reset = {
+                        let mut reset_target = reset_target;
+                        let mut reset_state = reset_state;
+                        let mut open_menu = open_menu;
+                        use_callback(move |deck_id| {
+                            open_menu.set(None);
+                            reset_state.set(ResetState::Idle);
+                            reset_target.set(Some(deck_id));
+                        })
+                    };
                     rsx! {
                         div { class: "practice-search history-search",
                             span { class: "practice-search-icon", aria_hidden: "true",
@@ -138,16 +160,83 @@ pub fn HistoryView() -> Element {
                         } else {
                             ul { class: "history-list",
                                 for card in visible_cards {
-                SummaryCard {
-                    key: "{card.id}",
-                    card,
-                    deck_label: deck_label.clone(),
-                    due_label: due_label.clone(),
-                    deck_id: deck_id.value(),
-                }
+                                    SummaryCard {
+                                        key: "{card.id}",
+                                        card,
+                                        deck_label: deck_label.clone(),
+                                        due_label: due_label.clone(),
+                                        deck_id: deck_id.value(),
+                                        open_menu,
+                                        on_reset,
+                                    }
+                                }
                             }
                         }
-                    }
+                        if open_menu().is_some() {
+                            div {
+                                class: "history-menu-overlay",
+                                onclick: move |_| open_menu.set(None),
+                            }
+                        }
+                        if let Some(deck_id) = reset_target() {
+                            div {
+                                class: "history-modal-overlay",
+                                onclick: move |_| {
+                                    reset_target.set(None);
+                                    reset_state.set(ResetState::Idle);
+                                },
+                                div {
+                                    class: "history-modal",
+                                    onclick: move |evt| evt.stop_propagation(),
+                                    h3 { class: "history-modal-title", "Reset deck learning?" }
+                                    p { class: "history-modal-body",
+                                        "This resets scheduling for every card in this deck."
+                                    }
+                                    if let ResetState::Error(err) = reset_state() {
+                                        p { class: "history-modal-error", "{err.message()}" }
+                                    }
+                                    div { class: "history-modal-actions",
+                                        button {
+                                            class: "btn editor-modal-cancel",
+                                            r#type: "button",
+                                            onclick: move |_| {
+                                                reset_target.set(None);
+                                                reset_state.set(ResetState::Idle);
+                                            },
+                                            "Cancel"
+                                        }
+                                        button {
+                                            class: "btn editor-modal-confirm",
+                                            r#type: "button",
+                                            disabled: reset_state() == ResetState::Resetting,
+                                            onclick: move |_| {
+                                                let mut reset_state = reset_state;
+                                                let mut reset_target = reset_target;
+                                                let mut resource = resource;
+                                                let card_service = card_service.clone();
+                                                spawn(async move {
+                                                    reset_state.set(ResetState::Resetting);
+                                                    match card_service
+                                                        .reset_deck_learning(DeckId::new(deck_id))
+                                                        .await
+                                                    {
+                                                        Ok(_) => {
+                                                            reset_state.set(ResetState::Idle);
+                                                            reset_target.set(None);
+                                                            resource.restart();
+                                                        }
+                                                        Err(_) => {
+                                                            reset_state.set(ResetState::Error(ViewError::Unknown));
+                                                        }
+                                                    }
+                                                });
+                                            },
+                                            "Reset"
+                                        }
+                                    }
+                                }
+                            }
+                        }
                     }
                 },
                 ViewState::Error(err) => rsx! {
@@ -173,9 +262,12 @@ fn SummaryCard(
     deck_label: String,
     due_label: String,
     deck_id: u64,
+    open_menu: Signal<Option<i64>>,
+    on_reset: EventHandler<u64>,
 ) -> Element {
     let navigator = use_navigator();
     let avatar = deck_label.chars().next().unwrap_or('D').to_string();
+    let is_open = open_menu() == Some(card.id);
     rsx! {
         li { class: "history-item",
             div { class: "history-item__main",
@@ -199,13 +291,51 @@ fn SummaryCard(
             div { class: "history-item__actions",
                 span { class: "history-item__date", "{card.completed_at_str}" }
                 span { class: "history-item__due", "{due_label}" }
-                button {
-                    class: "btn btn-primary history-item__action",
-                    r#type: "button",
-                    onclick: move |_| {
-                        let _ = navigator.push(Route::Session { deck_id });
-                    },
-                    "Practice again"
+                div { class: "history-action",
+                    button {
+                        class: "btn btn-primary history-item__action",
+                        r#type: "button",
+                        onclick: move |_| {
+                            if is_open {
+                                open_menu.set(None);
+                            } else {
+                                open_menu.set(Some(card.id));
+                            }
+                        },
+                        span { "Practice again" }
+                        span { class: "history-action-caret" }
+                    }
+                    if is_open {
+                        div { class: "history-action-menu",
+                            button {
+                                class: "history-action-item",
+                                r#type: "button",
+                                onclick: move |_| {
+                                    open_menu.set(None);
+                                    let _ = navigator.push(Route::Session { deck_id });
+                                },
+                                "Practice Due Cards"
+                            }
+                            button {
+                                class: "history-action-item",
+                                r#type: "button",
+                                onclick: move |_| {
+                                    open_menu.set(None);
+                                    let _ = navigator.push(Route::SessionAll { deck_id });
+                                },
+                                "Practice All Cards"
+                            }
+                            button {
+                                class: "history-action-item history-action-item--danger",
+                                r#type: "button",
+                                onclick: move |_| {
+                                    open_menu.set(None);
+                                    on_reset.call(deck_id);
+                                },
+                                "Reset Learning Progress"
+                            }
+                        }
+                    }
                 }
             }
         }
